c37e714ac930a486a97de6df578d23b2e3eca0ba
Nigel <nigelsilva12@gmail.com>
2018-11-13 17:45:47 -0500

    Add django-heroku to settings.py

--- lionheart/lionheart/settings.py
+++ lionheart/lionheart/settings.py
@@ -131,3 +131,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 SESSION_SAVE_EVERY_REQUEST = True
+
+# Configure Django App for Heroku.
+import django_heroku
+django_heroku.settings(locals())
