c53a15d465a223904ddfe7801ef5cad78cebcb2f
Nigel <nigelsilva12@gmail.com>
2018-11-20 22:09:20 -0500

    Update DB connect

--- lionheart/settings.py
+++ lionheart/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 
+
+import dj_database_url
+
 # Define "SETTINGS_PATH"
 SETTINGS_PATH = os.path.dirname(os.path.dirname(__file__))
 
@@ -85,16 +88,16 @@ WSGI_APPLICATION = 'lionheart.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'lionheart_database',
-        'USER': 'lionheart_db',
-        'PASSWORD': 'NigelPeter729',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
+# DATABASES = {
+#   'default': {
+#       'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#       'NAME': 'lionheart_database',
+#       'USER': 'lionheart_db',
+#       'PASSWORD': 'NigelPeter729',
+#       'HOST': 'localhost',
+#       'PORT': '5432',
+#   }
+# }
 
 
 # Password validation
@@ -138,3 +141,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'lionheart/static'),
 )
+
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
