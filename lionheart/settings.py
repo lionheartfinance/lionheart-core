a86cb55b77bc80f706e124c4b82728184e83699b
Nigel <nigelsilva12@gmail.com>
2018-11-20 22:10:29 -0500

    Update lionheart/settings.py

--- lionheart/settings.py
+++ lionheart/settings.py
@@ -88,16 +88,16 @@ WSGI_APPLICATION = 'lionheart.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-# DATABASES = {
-#   'default': {
-#       'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#       'NAME': 'lionheart_database',
-#       'USER': 'lionheart_db',
-#       'PASSWORD': 'NigelPeter729',
-#       'HOST': 'localhost',
-#       'PORT': '5432',
-#   }
-# }
+ DATABASES = {
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql_psycopg2',
+       'NAME': 'lionheart_database',
+       'USER': 'lionheart_db',
+       'PASSWORD': 'NigelPeter729',
+       'HOST': 'localhost',
+       'PORT': '5432',
+   }
+ }
 
 
 # Password validation
